f627690e816650f632b074ec29e20b64f144445a
darjss <darjsavid@gmail.com>
2025-10-14 10:40:37 +0800

    "fix search"

--- apps/api/src/app/trpc/router.ts
+++ apps/api/src/app/trpc/router.ts
@@ -5,57 +5,63 @@ import { TRPCError } from '@trpc/server';
 
 export const t = initTRPC.create();
 export const appRouter = t.router({
-  getAllBusinesses: t.procedure.input(z.object({
-    search: z.string().optional(),
-    categoryId: z.string().optional(),
-  })).query(async ({input}) => {
-    const businesses = await prisma.business.findMany({
-      where:{
-        ...(input.search && {
-          name: {
-            contains: input.search,
-            mode: 'insensitive',
+  getAllBusinesses: t.procedure
+    .input(
+      z.object({
+        search: z.string().optional(),
+        categoryId: z.string().optional(),
+      })
+    )
+    .query(async ({ input }) => {
+      const businesses = await prisma.business.findMany({
+        where: {
+          ...(input.search && {
+            name: {
+              contains: input.search,
+              mode: 'insensitive',
+            },
+          }),
+          ...(input.categoryId !== 'All' && {
+            categoryId: input.categoryId,
+          }),
+        },
+        include: {
+          category: {
+            select: { name: true },
           },
-          include: {
-            category: {
-              select: { name: true }
-            }
-          }
-        }),
-        ...(input.categoryId!=='All' && {
-          categoryId: input.categoryId,
-        }),
-      },
-      include: {
-        category: {
-          select: { name: true }
-        }
-      }
-    });
+        },
+      });
 
-    return businesses;
-  }),
+      return businesses;
+    }),
   getAllCategories: t.procedure.query(async (opts) => {
     const categories = await prisma.category.findMany();
     return categories;
   }),
-  getBusinessById: t.procedure.input(z.object({
-    id: z.string(),
-  })).query(async ({input}) => {
-    const business = await prisma.business.findUnique({
-      where: { id: input.id },
-      include: {
-        category: {
-          select: { name: true }
-        }
+  getBusinessById: t.procedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(async ({ input }) => {
+      const business = await prisma.business.findUnique({
+        where: { id: input.id },
+        include: {
+          category: {
+            select: { name: true },
+          },
+        },
+      });
+      if (!business) {
+        throw new TRPCError({
+          code: 'NOT_FOUND',
+          message: 'Business not found',
+        });
       }
-    });
-    if (!business) {
-      throw new TRPCError({ code: 'NOT_FOUND', message: 'Business not found' });
-    }
 
-    return business ;
-  }),
+      return business;
+    }),
 });
 
 export type AppRouter = typeof appRouter;
